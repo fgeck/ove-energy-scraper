f65e9c83dc5336659c0e2fa57de516e8bdc48763
Florian Geckeler <f.geckeler@sap.com>
2023-11-16 16:37:50 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,115 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://sem01.dvs.net/SEM.Portal.OVE/Login.aspx"
+query_params = {
+    'TestingCookie': 'true',
+    'ReturnUrl': '/SEM.Portal.OVE/Default.aspx',
+    '__VIEWSTATE': '/wEPDwUJLTYzMzgwMzc3DxYCHgtsb2dpblNlY0tleQUkYmY3ZWE1NWQtNDhkYS00MmY1LWI5YTItNzk0ZDNkYjBlMmVmFgQCAQ9kFgJmD2QWAmYPFgIeBFRleHQFQk9WRSBPYmpla3QtVmVyc29yZ3VuZyBtaXQgcmF0aW9uZWxsZW0gRW5lcmdpZWVpbnNhdHogR21iSCAmIENvLiBLR2QCAw9kFggCAg8PZBYCHgVzdHlsZQUNZGlzcGxheTpub25lO2QCAw8UKwACFCsAA2RkZGRkAgQPZBYEAgMPDxYCHgdWaXNpYmxlZ2RkAgkPPCsACgEADxYGHghDc3NDbGFzcwUPcmZkTG9naW5Db250cm9sHhBSZW5kZXJPdXRlclRhYmxlZx4EXyFTQgICZBYCZg9kFgICEg9kFgJmD2QWAgIBDxAPFgIeB0NoZWNrZWRoZGRkZAIGDxYCHwEF+wE8ZGl2IGlkPSJjb3B5cmlnaHQiPg0KICAgIDxiPk9WRSBPYmpla3QtVmVyc29yZ3VuZyBtaXQgcmF0aW9uZWxsZW0gRW5lcmdpZWVpbnNhdHogR21iSCAmYW1wOyBDby4gS0c8L2I+PGJyIC8+DQogICAgQW0gUGFnZW5rYW1wIDEx4oCTMTUsIDQ5MjE0IEJhZCBSb3RoZW5mZWxkZTxiciAvPg0KCUUtTWFpbDogPGEgaHJlZj0ibWFpbHRvOmVuZXJnaWVkYXRlbkBvdmUuZGUiPmVuZXJnaWVkYXRlbkBvdmUuZGU8L2E+PGJyIC8+DQo8L2Rpdj4NCmQYAQUeX19Db250cm9sc1JlcXVpcmVQb3N0QmFja0tleV9fFgIFEVJhZEZvcm1EZWNvcmF0b3IxBRFSYWRXaW5kb3dNYW5hZ2VyMS6bIJu/qIEnvFn1sPyuNqKB8XyFsM2G1jnJUw3CcnLY',
+    '__EVENTVALIDATION': '/wEdAAXTns6yuh6ImPbSU3JqZv1h8x5TPe4Fb2SCxWQFXQqD6Fz4Ff/mRdr9eJovHJ26GXAERP69Ut0BJHE40kfR6snh0LfbvGI/Wk1EnCpPRzC0arlQE7DBwzXqJJYPaVFg7hRui1/xQ6F8wiFn5wjJ6dS5',
+
+
+}
+form_data = {
+    'Login1$UserName': 'D208735',
+    'Login1$Password': '4Y^gZ.=',
+    'Login1$LoginButton': 'Anmelden'
+}
+
+cookies = {
+    'ASP.NET_SessionId': '4fluvfnz3pl5di4h0xamejgr',
+}
+
+headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Accept-Language': 'en-GB,en;q=0.9',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'keep-alive',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    # 'Cookie': 'ASP.NET_SessionId=4fluvfnz3pl5di4h0xamejgr',
+    'Origin': 'https://sem01.dvs.net',
+    'Referer': 'https://sem01.dvs.net/SEM.Portal.OVE/Login.aspx?TestingCookie=true&ReturnUrl=%2fSEM.Portal.OVE%2fHelp.aspx',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-User': '?1',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Mobile Safari/537.36',
+    'sec-ch-ua': '"Google Chrome";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-platform': '"Android"',
+}
+
+data = {
+   # 'RadStyleSheetManager1_TSSM': ';Telerik.Web.UI, Version=2012.3.1308.40, Culture=neutral, PublicKeyToken=121fae78165ba3d4:de-DE:0c62baaa-fecc-4eb5-91ff-e3de59bf8f40:ef4a543:67d175d:92753c09:91f742eb',
+   # 'RadScriptManager1_TSM': ';;System.Web.Extensions, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35:de-DE:c7c66246-7597-47ee-87ae-ac254004a457:ea597d4b:b25378d2;Telerik.Web.UI:de-DE:0c62baaa-fecc-4eb5-91ff-e3de59bf8f40:16e4e7cd:f7645509:22a6274a:ed16cbdc:86526ba7:874f8ea2:24ee1bba:f46195d3:19620875:490a9d4e:bd8f85e4',
+    '__EVENTTARGET': '',
+    '__EVENTARGUMENT': '',
+    '__VIEWSTATE': '/wEPDwUJLTYzMzgwMzc3DxYCHgtsb2dpblNlY0tleQUkZjY1MTVlODgtNWJjYS00YTQ3LWFmOTgtNDI3MzZlOWNiOTJlFgQCAQ9kFgJmD2QWAmYPFgIeBFRleHQFQk9WRSBPYmpla3QtVmVyc29yZ3VuZyBtaXQgcmF0aW9uZWxsZW0gRW5lcmdpZWVpbnNhdHogR21iSCAmIENvLiBLR2QCAw9kFggCAg8PZBYCHgVzdHlsZQUNZGlzcGxheTpub25lO2QCAw8UKwACFCsAA2RkZGRkAgQPZBYEAgMPDxYCHgdWaXNpYmxlZ2RkAgkPPCsACgEADxYGHghDc3NDbGFzcwUPcmZkTG9naW5Db250cm9sHhBSZW5kZXJPdXRlclRhYmxlZx4EXyFTQgICZBYCZg9kFgICEg9kFgJmD2QWAgIBDxAPFgIeB0NoZWNrZWRoZGRkZAIGDxYCHwEF+wE8ZGl2IGlkPSJjb3B5cmlnaHQiPg0KICAgIDxiPk9WRSBPYmpla3QtVmVyc29yZ3VuZyBtaXQgcmF0aW9uZWxsZW0gRW5lcmdpZWVpbnNhdHogR21iSCAmYW1wOyBDby4gS0c8L2I+PGJyIC8+DQogICAgQW0gUGFnZW5rYW1wIDEx4oCTMTUsIDQ5MjE0IEJhZCBSb3RoZW5mZWxkZTxiciAvPg0KCUUtTWFpbDogPGEgaHJlZj0ibWFpbHRvOmVuZXJnaWVkYXRlbkBvdmUuZGUiPmVuZXJnaWVkYXRlbkBvdmUuZGU8L2E+PGJyIC8+DQo8L2Rpdj4NCmQYAQUeX19Db250cm9sc1JlcXVpcmVQb3N0QmFja0tleV9fFgIFEVJhZEZvcm1EZWNvcmF0b3IxBRFSYWRXaW5kb3dNYW5hZ2VyMWc9zZfR8GW9VFAkW8p5mJGzU4IdRwnKx5ux+MD/X2/2',
+    '__EVENTVALIDATION': '/wEdAAXZ8zFj3pl6KRPcSL8CDrBl8x5TPe4Fb2SCxWQFXQqD6Fz4Ff/mRdr9eJovHJ26GXAERP69Ut0BJHE40kfR6snh0LfbvGI/Wk1EnCpPRzC0anbJIZpJdp41efoqM7tF7K7cJxyD+5VzgckgWIcPc8KM',
+    'RadFormDecorator1_ClientState': '',
+    'RadWindowManager1_ClientState': '',
+    'Login1$UserName': '',
+    'Login1$Password': '',
+    'Login1$LoginButton': 'Anmelden',
+}
+
+response = requests.post(
+    'https://sem01.dvs.net/SEM.Portal.OVE/Login.aspx',
+    params=query_params,
+    cookies=cookies,
+    headers=headers,
+    data=data,
+    allow_redirects=True
+)
+
+# Prints the HTTP response code
+print(response.status_code)
+
+with open('result.html', 'w') as f:
+    f.write(response.text)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+allGaugeDivs = soup.findAll('div', class_='gaugelabel')
+
+def getConsumptionFromText(text: str) -> float:
+    match = re.search(r'Wert: (\d+(,\d+)?)', text)
+    if match:
+        return float(match.group(1).replace(',', '.'))
+
+
+# electricity
+electricityLastMonth = getConsumptionFromText(allGaugeDivs[0].text)
+electricityThisMonth = getConsumptionFromText(allGaugeDivs[1].text)
+# heat
+heatLastMonth = getConsumptionFromText(allGaugeDivs[2].text)
+heatThisMonth = getConsumptionFromText(allGaugeDivs[3].text)
+# water
+waterLastMonth = getConsumptionFromText(allGaugeDivs[4].text)
+waterThisMonth = getConsumptionFromText(allGaugeDivs[5].text)
+
+print(
+    f"""
+    Consumption Report from OVE
+    ----------------------------
+    Electricity:
+    ------------
+    Last month: {electricityLastMonth} kWh
+    This month: {electricityThisMonth} kWh
+            
+    ----------------------------
+    HEATING:
+    ------------
+    Last month: {heatLastMonth} kWh
+    This month: {heatThisMonth} kWh
+    
+    ----------------------------
+    WATER:
+    ------------
+    Last month: {waterLastMonth} m³
+    This month: {waterThisMonth} m³
+
+    """
+)
